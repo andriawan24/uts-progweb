78f47bd41e2a2bd4f40c28d419a9d98eeac0b8bc
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2022-03-29 22:05:15 +0700

    Add detail page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,4 +18,59 @@ $(document).ready(function(){
             }
         }
     });
-  });
+
+    $('#card-promos').click(() => {
+        window.location.href = "/detail.html"
+    })
+
+    let input = $('#amount-input')
+    console.log(input)
+
+    $('#select-amount-1').click(() => {
+        input.val('')
+        let pTag = $("#select-amount-1 p:first")
+        let p2Tag = $("#select-amount-2 p:first")
+        let p3Tag = $("#select-amount-3 p:first")
+        if (pTag.hasClass('selected')) {
+            pTag.removeClass('selected')
+            input.prop('disabled', false)
+        } else {
+            pTag.addClass('selected')
+            p2Tag.removeClass('selected')
+            p3Tag.removeClass('selected')
+            input.prop('disabled', true)
+        }
+    })
+
+    $('#select-amount-2').click(() => {
+        input.val('')
+        let pTag = $("#select-amount-2 p:first")
+        let p2Tag = $("#select-amount-1 p:first")
+        let p3Tag = $("#select-amount-3 p:first")
+        if (pTag.hasClass('selected')) {
+            pTag.removeClass('selected')
+            input.prop('disabled', false)
+        } else {
+            pTag.addClass('selected')
+            p2Tag.removeClass('selected')
+            p3Tag.removeClass('selected')
+            input.prop('disabled', true)
+        }
+    })
+
+    $('#select-amount-3').click(() => {
+        input.val('')
+        let pTag = $("#select-amount-3 p:first")
+        let p2Tag = $("#select-amount-2 p:first")
+        let p3Tag = $("#select-amount-1 p:first")
+        if (pTag.hasClass('selected')) {
+            pTag.removeClass('selected')
+            input.prop('disabled', false)
+        } else {
+            pTag.addClass('selected')
+            p2Tag.removeClass('selected')
+            p3Tag.removeClass('selected')
+            input.prop('disabled', true)
+        }
+    })
+});
